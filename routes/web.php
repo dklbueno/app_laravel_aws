99e28b16403d7d6acd0a26301c9ff077a87a117f
Daniel Bueno <dkbueno@gmail.com>
2026-05-14 11:51:08 -0300

    Migração para php 8

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UploadController;
-use App\Http\Controllers\ProductController;
 
 Route::get('/', function () {
-    return view('upload');
+    return view('welcome');
 });
-
-Route::post('/upload', [UploadController::class, 'store'])->name('upload.store');
-
-Route::get('/', function () {
-    return redirect()->route('products.index');
-});
-
-Route::resource('products', ProductController::class);
