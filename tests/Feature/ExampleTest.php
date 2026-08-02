9bf49fed37485f683056384a8ee0300ac5acd728
Daniel Bueno <dkbueno@gmail.com>
2026-08-02 15:33:11 -0300

    Adicionando CI/CD com teste falhando

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,10 +10,15 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
+    // public function test_the_application_returns_a_successful_response(): void
+    // {
+    //     $response = $this->get('/');
 
-        $response->assertStatus(200);
+    //     $response->assertStatus(200);
+    // }
+
+    public function test_ci_is_working(): void
+    {
+        $this->assertTrue(false);
     }
 }
